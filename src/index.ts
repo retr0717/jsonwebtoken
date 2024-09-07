58bb93533867a26e39ffbee990c3243c8d207bbc
retr0717 <alwinaji717@gmail.com>
2024-09-07 22:09:37 +0530

    auth using jsonwebtoken

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
-
+const dotnev = require("dotenv");
+dotnev.config();
+const jwt = require("jsonwebtoken");
 //middlewares
 app.use(express.json());
 
 interface User {
   email: string;
   password: string;
-  token: string;
 }
 
-let users: User[] = [{ email: "alwin@gmail.com", password: "1234", token: "" }];
+let users: User[] = [{ email: "alwin@gmail.com", password: "1234" }];
 
 const generatToken = (email: string) => {
   const token = Math.random().toString(36);
@@ -19,7 +20,9 @@ const generatToken = (email: string) => {
 
 app.get("/auth", (req: any, res: any) => {
   const token = req.headers["token"] as string;
-  const user = users.find((user: any) => user.token === token);
+  const decodedInfo = jwt.verify(token, process.env.JWT_SECRET as string);
+  const user = users.find((user: any) => user.email === decodedInfo.email);
+
   if (!user) {
     return res.json({ message: "Invalid token" });
   }
@@ -34,13 +37,11 @@ app.post("/signin", (req: any, res: any) => {
   if (!user) {
     return res.json({ message: "Invalid email" });
   }
+
   if (user?.password !== password) {
     res.json({ message: "Invalid password" });
   }
-
-  const token = generatToken(email);
-  user.token = token;
-
+  const token = jwt.sign({ email }, process.env.JWT_SECRET as string);
   console.log(users);
 
   res.json({ message: "signin done", token: token });
@@ -60,7 +61,6 @@ app.post("/signup", (req: any, res: any) => {
   users.push({
     email,
     password,
-    token: "",
   });
 
   res.json({ message: "signin done", token: "" });
